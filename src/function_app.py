4deb4689efaf1ef011dc0fac4106b144d95b27d2
CHRISTIANO PICCININ <christiano.piccinin26@univille.br>
2026-05-07 19:42:52 -0300

    adicionado 3 functions

--- src/function_app.py
+++ src/function_app.py
@@ -1,14 +1,15 @@
 import logging
 import azure.functions as func
 
-
 # Ponto de entrada do Function App.
 # Cada trigger é registrado como uma função independente.
 app = func.FunctionApp()
 
 # Importa triggers para registrar as functions no app
-from triggers.extract_trigger import app as extract_trigger
+from triggers.extract_cliente import app as extract_cliente
+from triggers.extract_pedido import app as extract_pedido
+from triggers.extract_entrega import app as extract_entrega
 
-app.register_functions(extract_trigger)
-logging.info("Azure Function App inicializado.")
+
+
 
